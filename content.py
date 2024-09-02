9e6351da6f3ab01d93fb4afe96153ac0d13b6661
Andreas Varotsis <andrea.varotsis@gmail.com>
2024-09-02 12:16:32 +0100

    fixed blog link

--- content.py
+++ content.py
@@ -1,6 +1,6 @@
 from fasthtml.common import *
 
-opening_para = '''Welcome to Copbot Online! This is a prototype project by [me](https://andreasthinks.me/about.html), that aims to help us understand how AI models and humans differ in their perception of risk. You'll be presented with a theoretical scenario, and asked to conduct an assessment - once you have submitted your response, you will see how you compare to a range of AI models. You can read more about CopBot [here](https://andreasthinks.me/posts/Copbot/explainer.html). If you're happy to take part, read through the guidance below to get started.
+opening_para = '''Welcome to Copbot Online! This is a prototype project by [me](https://andreasthinks.me/about.html), that aims to help us understand how AI models and humans differ in their perception of risk. You'll be presented with a theoretical scenario, and asked to conduct an assessment - once you have submitted your response, you will see how you compare to a range of AI models. You can read more about CopBot [here](https://andreasthinks.me/posts/copbot_online/). If you're happy to take part, read through the guidance below to get started.
 
 Missing people in England and Wales are assessed based on guidance from [the College of Policing](https://www.college.police.uk/app/major-investigation-and-public-protection/missing-persons), the professional body for policing. The guidance is designed to help police officers and staff assess the risk to a missing person, and to help them decide on the appropriate response. '''
 
